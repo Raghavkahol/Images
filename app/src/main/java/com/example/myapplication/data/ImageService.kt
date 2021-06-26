d64606453f72a6e1370e283137381498ff583489
raghavkahol <raghav.kahol@games24x7.com>
2021-06-26 15:43:56 +0530

    API+KEY shifted to gradle.properties

--- app/src/main/java/com/example/myapplication/data/ImageService.kt
+++ app/src/main/java/com/example/myapplication/data/ImageService.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.data
 
+import com.example.myapplication.BuildConfig
 import com.example.myapplication.data.model.FlickerData
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -12,7 +13,7 @@ interface ImageService {
 
     @GET("services/rest?method=flickr.photos.getRecent&nojsoncallback=1&format=json")
     suspend fun getImages(
-        @Query("api_key") apiKey: String = "062a6c0c49e4de1d78497d13a7dbb360",
+        @Query("api_key") apiKey: String = BuildConfig.API_KEY,
         @Query("text") text: String?,
         @Query("per_page") perPage : Int = 5,
         @Query("page") page: Int
